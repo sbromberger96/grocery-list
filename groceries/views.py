a7bfa7dfb5ed970ddb1c74237d217610ac1bc10c
sophiebromberger <sophie5@vt.edu>
2017-10-21 14:17:04 -0400

    Implement Django templates for listing items

--- groceries/views.py
+++ groceries/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from django.template import loader
+
 from .models import Item
 
 
 def index(request):
     grocery_list = Item.objects.order_by('name')
-    return HttpResponse(grocery_list)
+    template = loader.get_template('items/index.html')
+    context = {
+        'grocery_list': grocery_list
+    }
+    return HttpResponse(template.render(context, request))
