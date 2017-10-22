35eb205dc524fc9347ae7c8f9bc03966214b4f68
sophiebromberger <sophie5@vt.edu>
2017-10-22 12:21:30 -0400

    Add item detail pages
    currently the detail pages just say the item's name
    in the future there will be delete functionality

--- groceries/views.py
+++ groceries/views.py
@@ -3,6 +3,8 @@ from django.template import Context
 from django.template.loader import get_template
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
 
 from .models import Item
 from groceries.forms import ItemForm
@@ -18,9 +20,29 @@ def new(request):
         if form.is_valid():
             item = form.save(commit=False)
             item.save()
-            return HttpResponseRedirect('/items/')
+            return HttpResponseRedirect('/items/new/')
     else:
         form = ItemForm()
         return render(request, 'items/new.html', 
             {'form': form,}
         )
+
+class ItemDetailView(DetailView):
+    model = Item
+    template_name = 'items/item-detail.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super(ItemDetailView, self).get_context_data(**kwargs)
+        return context
+
+
+class ItemListView(ListView):
+    template_name = 'items/index.html'
+    model = Item
+    context_object_name = 'items'
+
+    def get_context_data(self, **kwargs):
+        context = super(ItemListView, self).get_context_data(**kwargs)
+        context['name'] = self.model.name
+        return context
+
